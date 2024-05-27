417974e2f9ca28adebf4b7e894f46691fe088566
Boss-MBD <151726184+Boss-MBD@users.noreply.github.com>
2024-05-27 09:02:40 +0000

    r'fudoilkj'r"

--- NDC.py
+++ NDC.py
@@ -5,6 +5,13 @@ class App:
         
         self.currentState = "title_screen"
 
+        self.key_up = pyxel.KEY_UP
+        self.key_down = pyxel.KEY_DOWN
+        self.key_left = pyxel.KEY_LEFT
+        self.key_right = pyxel.KEY_RIGHT
+
+        self.timer = 60
+
         pyxel.init(256, 256, title="NDC")
         pyxel.run(self.update, self.draw)
 
@@ -18,9 +25,34 @@ class App:
     #Ladder    = 4
 
     def update_title_screen(self):
-        pass
+        if pyxel.btn(pyxel.KEY_RETURN):
+            self.currentState = "in_game"
+
+        if pyxel.btn(pyxel.KEY_UP):
+            self.key_up = pyxel.KEY_UP
+            self.key_down = pyxel.KEY_DOWN
+            self.key_left = pyxel.KEY_LEFT
+            self.key_right = pyxel.KEY_RIGHT
+        elif pyxel.btn(pyxel.KEY_Z):
+            self.key_up = pyxel.KEY_Z
+            self.key_down = pyxel.KEY_S
+            self.key_left = pyxel.KEY_Q
+            self.key_right = pyxel.KEY_D
+        elif pyxel.btn(pyxel.KEY_W):
+            self.key_up = pyxel.KEY_W
+            self.key_down = pyxel.KEY_S
+            self.key_left = pyxel.KEY_A
+            self.key_right = pyxel.KEY_D
+
+        if pyxel.btn(pyxel.KEY_1):
+            self.timer = 90
+        elif pyxel.btn(pyxel.KEY_2):
+            self.timer = 60
+        elif pyxel.btn(pyxel.KEY_3):
+            self.timer = 30
+
+
     def draw_title_screen(self): 
-        pyxel.rect(0, 0, 16, 16, 3)
-        pyxel.text(0, 16, "Yes", 3)
+
 
 App()
