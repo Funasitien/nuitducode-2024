3aa572cb6ff3ae3566419a9d30d9d4962e33de5e
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-05-27 09:44:46 +0200

    Create NDC.py

---
+++ NDC.py
@@ -0,0 +1,12 @@
+import pyxel
+
+class App:
+    def __init__(self) -> None:
+        
+        self.currentState = ""
+
+        pyxel.init(128, 128, title="NDC")
+        pyxel.run(self.update, self.draw)
+
+    def update(self): getattr(self, f"update_{self.currentState}")
+    def draw(self): getattr(self, f"draw_{self.currentState}")
