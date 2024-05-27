29f16c25549d9bf6a3185598abd2220df54f8976
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-05-27 10:25:36 +0200

    Update NDC.py

--- NDC.py
+++ NDC.py
@@ -5,13 +5,21 @@ class App:
         
         self.currentState = ""
 
-        pyxel.init(128, 128, title="NDC")
+        pyxel.init(256, 256, title="NDC")
         pyxel.run(self.update, self.draw)
 
-    def update(self): getattr(self, f"update_{self.currentState}")
-    def draw(self): getattr(self, f"draw_{self.currentState}")
+    def update(self): getattr(self, f"update_{self.currentState}")()
+    def draw(self): getattr(self, f"draw_{self.currentState}")()
+
+    #Air       = 0
+    #Chest     = 1
+    #Breakable = 2
+    #Unbreak   = 3 
+    #Ladder    = 4
 
     def update_(self): pass
-    def draw_(self): pass
+    def draw_(self): 
+        pyxel.rect(0, 0, 16, 16, 3)
+        pyxel.text(0, 16, "Yes", 3)
 
 App()
